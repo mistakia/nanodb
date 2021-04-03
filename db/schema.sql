44841ffe564943afb322bc7dd08fb5daa1ac2d19
mistakia <1823355+mistakia@users.noreply.github.com>
2021-04-02 21:35:13 -0400

    fix: use char instead of varchar for fixed length content

--- db/schema.sql
+++ db/schema.sql
@@ -7,16 +7,16 @@
 DROP TABLE IF EXISTS `accounts`;
 
 CREATE TABLE `accounts` (
-  `account` varchar(65) NOT NULL,
-  `frontier` varchar(64) DEFAULT NULL,
-  `open_block` varchar(64) DEFAULT NULL,
-  `representative_block` varchar(64) DEFAULT NULL,
+  `account` char(65) NOT NULL,
+  `frontier` char(64) DEFAULT NULL,
+  `open_block` char(64) DEFAULT NULL,
+  `representative_block` char(64) DEFAULT NULL,
   `balance` varchar(39) DEFAULT NULL,
   `modified_timestamp` int(11) DEFAULT NULL,
   `block_count` int(11) DEFAULT NULL,
   `confirmation_height` int(11) DEFAULT NULL,
-  `confirmation_height_frontier` varchar(64) DEFAULT NULL,
-  `key` varchar(64) DEFAULT NULL,
+  `confirmation_height_frontier` char(64) DEFAULT NULL,
+  `key` char(64) DEFAULT NULL,
   UNIQUE KEY `account` (`account`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -29,20 +29,20 @@ CREATE TABLE `accounts` (
 DROP TABLE IF EXISTS `blocks`;
 
 CREATE TABLE `blocks` (
-  `hash` varchar(64) NOT NULL,
+  `hash` char(64) NOT NULL,
   `amount` varchar(39) NOT NULL,
   `balance` varchar(39) NOT NULL,
   `height` int(11) NOT NULL,
   `local_timestamp` int(11) NOT NULL,
   `confirmed` tinyint(1) NOT NULL,
   `type` tinyint(1) NOT NULL,
-  `account` varchar(65) NOT NULL,
-  `previous` varchar(64) NOT NULL,
-  `representative` varchar(65) NOT NULL,
-  `link` varchar(64) NOT NULL,
-  `link_as_account` varchar(65) NOT NULL,
-  `signature` varchar(128) NOT NULL,
-  `work` varchar(16) NOT NULL,
+  `account` char(65) NOT NULL,
+  `previous` char(64) NOT NULL,
+  `representative` char(65) NOT NULL,
+  `link` char(64) NOT NULL,
+  `link_as_account` char(65) NOT NULL,
+  `signature` char(128) NOT NULL,
+  `work` char(16) NOT NULL,
   `subtype` tinyint(1) NOT NULL,
   UNIQUE KEY `block` (`hash`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
