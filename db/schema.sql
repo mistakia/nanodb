fc2d029363f7920d49cf1b1cb9f4603807beb22f
mistakia <1823355+mistakia@users.noreply.github.com>
2021-04-11 16:19:11 -0400

    fix: update schema

--- db/schema.sql
+++ db/schema.sql
@@ -30,15 +30,15 @@ DROP TABLE IF EXISTS `blocks`;
 
 CREATE TABLE `blocks` (
   `hash` char(64) NOT NULL,
-  `amount` varchar(39) NOT NULL,
-  `balance` varchar(39) NOT NULL,
+  `amount` varchar(39) DEFAULT NULL,
+  `balance` varchar(39) DEFAULT NULL,
   `height` int(11) NOT NULL,
   `local_timestamp` int(11) NOT NULL,
   `confirmed` tinyint(1) NOT NULL,
   `type` tinyint(1) NOT NULL,
   `account` char(65) NOT NULL,
   `previous` char(64) NOT NULL,
-  `representative` char(65) NOT NULL,
+  `representative` char(65) DEFAULT NULL,
   `link` char(64) NOT NULL,
   `link_as_account` char(65) NOT NULL,
   `signature` char(128) NOT NULL,
