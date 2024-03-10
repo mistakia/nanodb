498fec644ee37013a3cfc8bb1e897099b06a7463
mistakia <1823355+mistakia@users.noreply.github.com>
2024-03-09 21:13:46 -0500

    feat: add endpoint for confirmation latency for each bucket

--- api/routes/blocks/index.mjs
+++ api/routes/blocks/index.mjs
@@ -4,11 +4,13 @@ import send from './send.mjs'
 // import change from './change.mjs'
 // import count from './count.mjs'
 import unconfirmed from './unconfirmed.mjs'
+import confirmed from './confirmed.mjs'
 
 const router = express.Router()
 
 router.use('/send', send)
 router.use('/unconfirmed', unconfirmed)
+router.use('/confirmed', confirmed)
 
 // router.use('/change', change)
 // router.use('/count', count)
