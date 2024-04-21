aaccf3f9d89f52913e41e6a32954e6a5446cbaa3
mistakia <1823355+mistakia@users.noreply.github.com>
2024-04-20 22:34:35 -0400

    feat: add `/api/accounts/balance_history` endpoint

--- api/routes/accounts/index.mjs
+++ api/routes/accounts/index.mjs
@@ -3,9 +3,11 @@ import express from 'express'
 import send from './send.mjs'
 import unconfirmed from './unconfirmed.mjs'
 import account_routes from './account/index.mjs'
+import balance_history from './balance_history.mjs'
 
 const router = express.Router({ mergeParams: true })
 
+router.use('/balance_history', balance_history)
 router.use('/send', send)
 router.use('/unconfirmed', unconfirmed)
 
