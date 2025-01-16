17de6a96b1789fe55da82fab7012d349cbb67e66
mistakia <1823355+mistakia@users.noreply.github.com>
2025-01-16 12:10:28 -0500

    chore: setup deploy

---
+++ server.pm2.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nanodb-api',
+      script: 'server.mjs',
+      watch: './api',
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      max_memory_restart: '2G'
+    },
+    {
+      name: 'nanodb-sync-websocket',
+      script: 'scripts/import-websocket.mjs',
+      watch: '.',
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      max_memory_restart: '2G'
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: 'storage',
+      ref: 'origin/master',
+      repo: 'https://github.com/mistakia/nanodb.git',
+      path: '/root/league',
+      'pre-deploy': 'git pull',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'source ~/.bash_profile && /home/user/.nvm/versions/node/v16.4.0/bin/yarn install && pm2 reload server.pm2.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+}
