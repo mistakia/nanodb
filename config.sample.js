a65a821a0b9bc06c9855bff9dd98728bfb097e45
mistakia <1823355+mistakia@users.noreply.github.com>
2026-06-08 21:55:35 -0400

    chore(config): drop retired job_tracker block from sample

--- config.sample.js
+++ config.sample.js
@@ -42,9 +42,5 @@ module.exports = {
       password: 'neo4j',
       port: '7687'
     }
-  },
-  job_tracker: {
-    api_url: 'https://storage-server:8081',
-    api_key: 'your-api-key-here'
   }
 }
